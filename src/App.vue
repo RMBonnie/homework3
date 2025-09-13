6dab434f5c3875f01e7c114d3e6bec99f07ae820
Juan Martinez <1101462jucamajr@gmail.com>
2025-09-13 11:35:11 -0700

    frist commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import {ref} from 'vue'
+  
+  const name = ref('')
+</script>
+
+<template>
+
+  <main class="app">
+    
+    <section class="greeting">
+      <h2 class="class">
+        Welcome back, <input type="text" placeholder="Enter Name" v-model="name">
+      </h2>
+
+    </section>
+
+    <section class="create-todo">
+
+
+    </section>
+    
+    <secion class="todo-list">
+
+
+    </secion>
+
+  </main>
+
+
+</template>
+
